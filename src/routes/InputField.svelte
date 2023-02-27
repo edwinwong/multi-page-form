9cf97ccb184e82a901746b62e988f89e975652ad
Edwin Wong <wong.edwin@gmail.com>
2023-02-26 20:35:41 -0500

    Initial commit

---
+++ src/routes/InputField.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let
+    value: string,
+    label: string,
+    type: string = 'text';
+
+  const id = label;
+
+  function typeAction(node: HTMLInputElement): void {
+    node.type = type;
+  }
+</script>
+
+<p class="form-control">
+  {#if label}
+    <label class="label" for={label}>{label}:</label>
+  {/if}
+  <input use:typeAction class="input" {id} bind:value />
+</p>
+
+<style>
+  .form-control {
+    margin: 0.5rem 0;
+    text-align: left;
+  }
+  .input {
+    width: 95%;
+    display: block;
+    padding: 0.5rem 0.5rem;
+    margin-top: 0.5rem;
+    border-width: 1px;
+    border-radius: 0.25rem;
+  }
+</style>
---
+++ src/routes/Validations.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { z } from "zod";
+  
+  // creating a schema for strings
+  const mySchema = z.string();
+  
+  mySchema.safeParse("tuna"); // => { success: true; data: "tuna" }
+  const result = mySchema.safeParse(12); // => { success: false; error: ZodError }
+  
+  if (!result.success) {
+    console.log(result.error.issues[0].message);
+  }
+  
+  let name = 'world';
+  </script>
+
+{#if result.success}
+  <p>
+    {result.data}
+  </p>
+{/if}
